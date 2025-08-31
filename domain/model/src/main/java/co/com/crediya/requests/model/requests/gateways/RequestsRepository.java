34996d2ffd8c5bce99536f2648b6cc3726cb9d04
Gabriel delgado <jegadele@hotmail.com>
2025-08-31 18:54:23 -0500

    refactor(requestsRepository): change Mono to Flux in RequestsRepository.java

--- domain/model/src/main/java/co/com/crediya/requests/model/requests/gateways/RequestsRepository.java
+++ domain/model/src/main/java/co/com/crediya/requests/model/requests/gateways/RequestsRepository.java
@@ -1,11 +1,12 @@
 package co.com.crediya.requests.model.requests.gateways;
 
 import co.com.crediya.requests.model.requests.Requests;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface RequestsRepository {
     Mono<Requests> save(Requests requests);
-    Mono<Requests> findByStatus(String status);
-    Mono<Requests> findByTypeLoan(String typeLoan);
-    Mono<Requests> findByEmail(String email);
+    Flux<Requests> findByStatus(String status);
+    Flux<Requests> findByTypeLoan(String typeLoan);
+    Flux<Requests> findByEmail(String email);
 }
