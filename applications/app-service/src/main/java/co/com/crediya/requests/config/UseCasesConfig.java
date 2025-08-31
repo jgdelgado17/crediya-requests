701580b2d5177a847f46680814c4fd879144da83
Gabriel delgado <jegadele@hotmail.com>
2025-08-30 21:13:08 -0500

    Init microservice crediya-requests

---
+++ applications/app-service/src/main/java/co/com/crediya/requests/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.crediya.requests.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.crediya.requests.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
