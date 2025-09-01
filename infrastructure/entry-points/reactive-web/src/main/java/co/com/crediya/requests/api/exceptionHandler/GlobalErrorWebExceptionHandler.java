89426f0d8082e878a66e2179fe2b299f0502b639
jgdelgado17 <jegadele@hotmail.com>
2025-09-01 10:23:04 -0500

    feat(GlobalErrorWebExceptionHandler): add global exception handler

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/requests/api/exceptionHandler/GlobalErrorWebExceptionHandler.java
@@ -0,0 +1,87 @@
+package co.com.crediya.requests.api.exceptionHandler;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import org.springframework.web.server.ServerWebInputException;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+@Order(-2)
+public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler {
+
+    public GlobalErrorWebExceptionHandler(ErrorAttributes errorAttributes,
+                                          WebProperties webProperties,
+                                          ApplicationContext applicationContext,
+                                          ServerCodecConfigurer configurer) {
+        super(errorAttributes, webProperties.getResources(), applicationContext); // <--- Se accede a getResources()
+        this.setMessageWriters(configurer.getWriters());
+        this.setMessageReaders(configurer.getReaders());
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+        Throwable error = getError(request);
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        String errorMessage = "Internal Server Error";
+
+        if (error instanceof WebExchangeBindException bindException) {
+            httpStatus = HttpStatus.BAD_REQUEST;
+            List<String> errors = bindException.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            errorMessage = "Validation failed: " + String.join(", ", errors);
+        } else if (error instanceof IllegalArgumentException) {
+            httpStatus = HttpStatus.BAD_REQUEST;
+            errorMessage = error.getMessage();
+        } else if (error instanceof ServerWebInputException) {
+            httpStatus = HttpStatus.BAD_REQUEST;
+            errorMessage = "Invalid JSON format or data type mismatch. Please check your input.";
+        } else if (error instanceof DataIntegrityViolationException){
+            httpStatus = HttpStatus.BAD_REQUEST;
+            errorMessage = "The request could not be processed due to a data integrity error.";
+
+            if (error.getCause() instanceof DuplicateKeyException) {
+                errorMessage = "The request could not be processed due to a duplicate key error.";
+            }
+        } else if (error instanceof DataAccessResourceFailureException) {
+            httpStatus = HttpStatus.SERVICE_UNAVAILABLE;
+            errorMessage = "Database service is unavailable. Please try again later.";
+        }
+
+        Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        //Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.of(Include.STACK_TRACE));
+        errorAttributes.put("status", httpStatus.value());
+        errorAttributes.put("error", httpStatus.getReasonPhrase());
+        errorAttributes.put("message", errorMessage);
+        errorAttributes.remove("exception");
+        errorAttributes.remove("path");
+        errorAttributes.remove("requestId");
+
+        return ServerResponse.status(httpStatus)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorAttributes));
+    }
+}
