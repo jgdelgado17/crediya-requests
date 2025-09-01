51efc71e92e16ea005196f246dd4511ae00d16e3
Gabriel delgado <jegadele@hotmail.com>
2025-08-31 20:37:50 -0500

    feat(RequestsUseCase): add RequestsUseCase and RequestsValidator

---
+++ domain/usecase/src/main/java/co/com/crediya/requests/usecase/requests/RequestsUseCase.java
@@ -0,0 +1,120 @@
+package co.com.crediya.requests.usecase.requests;
+
+import co.com.crediya.requests.model.requests.Requests;
+import co.com.crediya.requests.model.requests.gateways.RequestsRepository;
+import co.com.crediya.requests.model.shared.exceptions.ErrorMessages;
+import co.com.crediya.requests.model.status.Status;
+import co.com.crediya.requests.model.status.StatusEnum;
+import co.com.crediya.requests.model.status.gateways.StatusRepository;
+import co.com.crediya.requests.model.typeloan.gateways.TypeLoanRepository;
+import co.com.crediya.requests.usecase.status.StatusValidator;
+import co.com.crediya.requests.usecase.typeloan.TypeLoanValidator;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class RequestsUseCase {
+    private final RequestsRepository requestsRepository;
+    private final TypeLoanRepository typeLoanRepository;
+    private final StatusRepository statusRepository;
+
+    /**
+     * Creates a new request with the given parameters.
+     *
+     * <p>This method validates the type loan and status, and if they exist, it creates a new request.
+     *
+     * @param request The request to be created.
+     * @return A Mono that emits a saved request or an error if the type loan or status is not found.
+     */
+    public Mono<Requests> createRequest(Requests request){
+        return typeLoanRepository.findByName(request.getTypeLoan().getName())
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Type loan not found in database")))
+                .zipWith(
+                        statusRepository.findByName(StatusEnum.PENDING_REVIEW.getValue())
+                                .switchIfEmpty(Mono.error(new Exception("Status not found in database")))
+                )
+                .flatMap(tuple -> {
+                    var typeLoan = tuple.getT1();
+                    var status = tuple.getT2();
+                    request.setTypeLoan(typeLoan);
+                    request.setStatus(status);
+                    return requestsRepository.save(request);
+                });
+    }
+
+    /**
+     * Finds all requests by status.
+     *
+     * <p>This method validates the status name and if it is valid, it finds all requests with the
+     * given status.
+     *
+     * @param status the status to be found. The status cannot be null or empty.
+     * @return a Flux that emits all requests with the given status or an error if the status name is
+     * invalid.
+     * @see StatusValidator#validateName(String)
+     */
+    public Flux<Requests> findRequestByStatus(String status){
+        return StatusValidator.validateName(status)
+                .flatMapMany(requestsRepository::findByStatus);
+    }
+
+    /**
+     * Finds all requests by email.
+     *
+     * <p>This method validates the email and if it is valid, it finds all requests with the
+     * given email.
+     *
+     * @param email the email to be found. The email cannot be null or empty.
+     * @return a Flux that emits all requests with the given email or an error if the email is
+     * invalid.
+     * @see RequestsValidator#validateEmail(String)
+     */
+    public Flux<Requests> findRequestByEmail(String email){
+        return RequestsValidator.validateEmail(email)
+                .flatMapMany(requestsRepository::findByEmail);
+    }
+
+    /**
+     * Finds all requests by type loan.
+     *
+     * <p>This method validates the type loan name and if it is valid, it finds all requests with the
+     * given type loan.
+     *
+     * @param typeLoan the type loan to be found. The type loan cannot be null or empty.
+     * @return a Flux that emits all requests with the given type loan or an error if the type loan name is
+     * invalid.
+     * @see TypeLoanValidator#validateName(String)
+     */
+    public Flux<Requests> findRequestByTypeLoan(String typeLoan){
+        return TypeLoanValidator.validateName(typeLoan)
+                .flatMapMany(requestsRepository::findByTypeLoan);
+    }
+
+    /**
+     * Updates the status of a request.
+     *
+     * <p>This method first finds a request by id and then finds a status by name.
+     * If the request is not found, an error is returned. If the status is not found,
+     * an error is returned. If the request and status are found, the status of the
+     * request is updated.
+     *
+     * @param id the id of the request to be updated.
+     * @param statusName the name of the status to be updated.
+     * @return a Mono that emits the updated request or an error if the request or status is not found.
+     */
+    public Mono<Requests> updateStatusRequest(Integer id, String statusName) {
+        return requestsRepository.findById(id)
+                .switchIfEmpty(Mono.error(new IllegalArgumentException(ErrorMessages.notFoundMessage(Requests.class, id))))
+                .zipWith(StatusValidator.validateName(statusName)
+                        .flatMap(status -> statusRepository.findByName(statusName)
+                                .switchIfEmpty(Mono.error(new IllegalArgumentException(ErrorMessages.notFoundMessage(Status.class, statusName))))
+                ))
+                .flatMap(tuple -> {
+                    var request = tuple.getT1();
+                    var status = tuple.getT2();
+                    request.setStatus(status);
+                    return requestsRepository.save(request);
+                });
+    }
+}
---
+++ domain/usecase/src/main/java/co/com/crediya/requests/usecase/requests/RequestsValidator.java
@@ -0,0 +1,24 @@
+package co.com.crediya.requests.usecase.requests;
+
+import co.com.crediya.requests.model.shared.exceptions.ErrorMessages;
+import reactor.core.publisher.Mono;
+
+public class RequestsValidator {
+
+    /**
+     * Validates an email.
+     *
+     * <p>This method checks if the email is null or empty. If the email is null or empty,
+     * a {@link Mono} that emits an error is returned. If the email is valid, then a
+     * {@link Mono} that emits the email is returned.
+     *
+     * @param email the email to be validated. The email cannot be null.
+     * @return a {@link Mono} that emits a valid email or an error.
+     */
+    public static Mono<String> validateEmail(String email){
+        if(email == null || email.isEmpty()){
+            return Mono.error(new IllegalArgumentException(ErrorMessages.requiredField("email")));
+        }
+        return Mono.just(email);
+    }
+}
