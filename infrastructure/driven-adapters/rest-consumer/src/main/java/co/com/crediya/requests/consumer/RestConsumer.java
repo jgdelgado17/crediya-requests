d5ed1298479f9705e96c464a08c59cc46f958aaa
Gabriel delgado <jegadele@hotmail.com>
2025-09-11 00:36:59 -0500

    refactor(application.yaml): add environments vars

--- infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/crediya/requests/consumer/RestConsumer.java
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/crediya/requests/consumer/RestConsumer.java
@@ -7,6 +7,7 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,13 +19,16 @@ public class RestConsumer implements UserGateway {
     private final WebClient client;
     private static final Logger log = LoggerFactory.getLogger(RestConsumer.class);
 
+    @Value("${adapter.restconsumer.uri-user}")
+    private String uriUser;
+
     @Override
     @CircuitBreaker(name = "validateEmailUser")
     public Mono<User> findUserByEmail(String email, String token) {
         log.info("Finding user by email: {}", email);
         return client
                 .get()
-                .uri("/users/{email}", email)
+                .uri(uriUser, email)
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .retrieve()
                 .bodyToMono(User.class)
